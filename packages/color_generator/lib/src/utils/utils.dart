6fdb561ff215e6b2c04056092fed9d2f9c4e365c
Muhammad Idrees <midrees097@gmail.com>
2023-06-22 23:23:08 +0500

    created a color cache

--- packages/color_generator/lib/src/utils/utils.dart
+++ packages/color_generator/lib/src/utils/utils.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:color_generator/color_generator.dart';
 import 'package:flutter/foundation.dart';
 
 part 'random_eight_bit_generator.dart';
+part 'color_cache.dart';
